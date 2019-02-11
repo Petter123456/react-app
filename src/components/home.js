a0dff7edf30ba59e6a6f3d5fee43be3df1eaac54
Petter Fagerlund <petter.fagerlund@gmail.com>
2019-02-11 23:39:45 +0100

    html for home page added

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+console.log('sökpersonal');
+
 const Home = () => {
     return(
+
         <div>
-        <p>home</p>
-        
+            <div>
+                <p>Bli anställd & få 10.000 kr</p>
+                <p>Marknadens lättaste, billigaste och bästa sätta för arbetsgivare att hitta personal</p>
+                <button>Hur funkar det?</button>
+            </div>
+            <div>
+                <p>Ladda up ditt cv och låt arbetsgivare hitta dig idag</p>
+                    <form>
+                        <span>&#x1F50E;</span>
+                            <input type="text" name="sokKompetens" placeholder="Sök kompetens"/>
+                        <span>&#127760;</span>
+                            <input type="text" name="location" placeholder="Vart i Sverige?"/>
+                            <input type="submit" value="Sök"/>
+                    </form>
+            </div> 
         </div>
     );
 }
